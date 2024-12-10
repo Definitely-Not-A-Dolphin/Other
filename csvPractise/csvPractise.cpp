f05b5405083524dc1f00383dc3141c0fcd6e091a
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2024-12-10 08:25:09 +0100

    getElement added

--- csvPractise/csvPractise.cpp
+++ csvPractise/csvPractise.cpp
@@ -1,20 +1,53 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
 #include <cmath>
+#include <fstream>
+#include <iostream>
 #include <list>
-#include <string>
 #include <sstream>
+#include <string>
+#include <vector>
 
 std::ifstream readFile("Words.csv");
 
+std::string getElement(int row, int column);
+
+void pointerMover(std::ifstream &file, int k);
 
 int main() {
 
-    std::string output;
-    std::getline(readFile, output);
+  std::cout << getElement(2, 4) << std::endl;
 
-    std::cout << output;
+  return 0;
+}
 
-    return 0;
+std::string getElement(int row, int column) {
+  pointerMover(readFile, row);
+
+  std::string output;
+  std::getline(readFile, output);
+
+  std::string tmp; // A string to store the word on each iteration.
+  std::stringstream str_strm(output);
+  std::vector<std::string> wordsVector; // Create vector to hold our words
+  while (str_strm >> tmp) {
+    tmp.pop_back();
+    wordsVector.push_back(tmp);
+  }
+
+  return wordsVector.at(column - 1);
+}
+
+void pointerMover(std::ifstream &file, int k) {
+
+  // Special thanks to Elses/Menium for writing this part!
+
+  file.clear();
+  file.seekg(0, std::ios::beg);
+
+  std::string line;
+  for (int currentLine = 1; currentLine < k; currentLine++) {
+    if (!std::getline(file, line)) {
+      std::cerr << "Error: Line " << k << " does not exist!" << std::endl;
+      return;
+    }
+  }
 }
