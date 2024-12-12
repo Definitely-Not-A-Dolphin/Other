3203abc4a8d48a8a65bb377d448ab5bb19658e6e
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2024-12-12 22:55:36 +0100

    Ill fix this tomorrow

--- csvPractise/csvPractise.cpp
+++ csvPractise/csvPractise.cpp
@@ -6,24 +6,41 @@
 #include <string>
 #include <vector>
 
-std::ifstream readFile("Words.csv");
+std::string getElement(std::string fileName, int row, int column);
 
-std::string getElement(int row, int column);
+int fileSize(std::string fileName);
 
-void pointerMover(std::ifstream &file, int k);
+void pointerMover(std::string fileName, int k);
 
 int main() {
 
-  std::cout << getElement(2, 4) << std::endl;
+  std::cout << getElement("Words2.csv", 4, 3) << std::endl;
+
+  std::cout << fileSize("Words1.csv") << std::endl;
 
   return 0;
 }
 
-std::string getElement(int row, int column) {
-  pointerMover(readFile, row);
+int fileSize(std::string fileName) {
+  std::ifstream file(fileName, std::ios::in);
+
+  int lineCounter = 0;
+  std::string output;
+
+  while (getline(file, output)) {
+    lineCounter += 1;
+  }
+  file.close();
+
+  return lineCounter;
+}
+
+std::string getElement(std::string fileName, int row, int column) {
+  std::ifstream file(fileName);
+  pointerMover(fileName, row);
 
   std::string output;
-  std::getline(readFile, output);
+  std::getline(file, output);
 
   std::string tmp; // A string to store the word on each iteration.
   std::stringstream str_strm(output);
@@ -33,12 +50,14 @@ std::string getElement(int row, int column) {
     wordsVector.push_back(tmp);
   }
 
+  file.close();
+
   return wordsVector.at(column - 1);
 }
 
-void pointerMover(std::ifstream &file, int k) {
-
+void pointerMover(std::string fileName, int k) {
   // Special thanks to Elses/Menium for writing this part!
+  std::ifstream file(fileName, std::ios::in);
 
   file.clear();
   file.seekg(0, std::ios::beg);
@@ -50,4 +69,5 @@ void pointerMover(std::ifstream &file, int k) {
       return;
     }
   }
+  file.close();
 }
