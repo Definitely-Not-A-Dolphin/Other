67782c3f8c9ce181c0a5eb4c180db1149e3dcfae
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2024-12-25 21:48:46 +0100

    Tried to make a better getElement

--- csvPractise/csvPractise.cpp
+++ csvPractise/csvPractise.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 #include <cmath>
 #include <fstream>
 #include <iostream>
@@ -5,6 +6,7 @@
 #include <random>
 #include <sstream>
 #include <string>
+
 #include <vector>
 
 std::string fileName;
@@ -12,23 +14,34 @@ std::string fileName;
 std::vector<std::string> fileNameVector = {"fileOne.csv", "fileTwo.csv",
                                            "fileThree.csv"};
 
-int fileSize(std::ifstream &file);
+void pointerMover(std::ifstream &file, int k) {
+
+  // Special thanks to Elses/Menium for writing this part!
+
+  file.clear();
+  file.seekg(0, std::ios::beg);
+
+  std::string line;
+  for (int currentLine = 1; currentLine < k; currentLine++) {
+    if (!std::getline(file, line)) {
+      std::cerr << "Error: Line " << k << " does not exist!" << std::endl;
+      return;
+    }
+  }
+}
+
+int fileSize(std::string fileName);
+
+std::string getElement(std::string fileName, int row, int column);
+std::string getElementChar(std::string fileName, int row, int column);
 
 int main() {
 
-  std::cin >> fileName;
-
-  std::ifstream readFile(fileName, std::ios::in);
-
-  std::cout << std::endl << fileSize(readFile);  
+  std::cout << getElementChar("Words1.csv", 2, 2) << std::endl;
 
   return 0;
 }
 
-<<<<<<< Updated upstream
-int fileSize(std::ifstream &file) {
-  int lineCount = 0;
-=======
 int fileSize(std::string fileName) {
   std::ifstream file(fileName, std::ios::in);
 
@@ -43,13 +56,54 @@ int fileSize(std::string fileName) {
   return lineCounter;
 }
 
-std::string getElement(std::ifstream &file, int row, int column) {
+std::string getElement(std::string fileName, int row, int column) {
+
+  std::ifstream file(fileName, std::ios::in);
+
   pointerMover(file, row);
->>>>>>> Stashed changes
 
   std::string output;
-  while (getline(file, output, '\n')) {
-    lineCount += 1;
-  }
-  return lineCount;
+  getline(file, output, ',');
+
+  std::string tmp; // A string to store the word on each iteration.
+  std::stringstream str_strm(output);
+  std::vector<std::string> wordsVector; // Create vector to hold our words
+  while (str_strm >> tmp) {
+    tmp.pop_back();
+    wordsVector.push_back(tmp);
+  };
+
+  file.close();
+
+  return wordsVector.at(column - 1);
+}
+
+std::string getElementChar(std::string fileName, int row, int column) {
+  std::ifstream file(fileName, std::ios::in);
+
+  pointerMover(file, row);
+
+  int counter = 0;
+
+  while (true) {
+
+    std::cout << "loop!" << std::endl;
+
+    char destination[36];
+
+    file.get(destination, 36, ',');
+
+    std::string output = destination;
+
+    counter++;
+
+    std::cout << counter << std::endl << column << std::endl;
+
+    if (counter == column) {
+      std::cout << output << std::endl;
+
+      file.close();
+      return output;
+    }
+  };
 }
